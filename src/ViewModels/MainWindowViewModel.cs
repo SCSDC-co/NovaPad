19a6a3423f537eac4f28aa28e376a34b7553eb0c
Giuliano <giulianodeamicis2009@gmail.com>
2025-08-22 21:45:53 +0200

    Added working line number

--- src/ViewModels/MainWindowViewModel.cs
+++ src/ViewModels/MainWindowViewModel.cs
@@ -1,3 +1,38 @@
-﻿namespace src.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Linq;
 
-public partial class MainWindowViewModel : ViewModelBase;
+namespace src.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private string _text = "";
+    public string Text
+    {
+        get => _text;
+        set
+        {
+            if (SetProperty(ref _text, value))
+                UpdateNumbers();
+        }
+    }
+
+    private string _lineNumbers = "1";
+    public string LineNumbers
+    {
+        get => _lineNumbers;
+        set => SetProperty(ref _lineNumbers, value);
+    }
+
+    private double _lineNumbersOffset;
+    public double LineNumbersOffset
+    {
+        get => _lineNumbersOffset;
+        set => SetProperty(ref _lineNumbersOffset, value);
+    }
+
+    private void UpdateNumbers()
+    {
+        var lineCount = string.IsNullOrEmpty(Text) ? 1 : Text.Split('\n').Length;
+        LineNumbers = string.Join("\n", Enumerable.Range(1, lineCount));
+    }
+}
--- src/Views/MainWindow.axaml.cs
+++ src/Views/MainWindow.axaml.cs
@@ -1,11 +1,55 @@
 using Avalonia.Controls;
+using Avalonia.VisualTree;
+using System.Linq;
+using Avalonia.Threading;
 
 namespace src.Views;
 
 public partial class MainWindow : Window
 {
+    private ScrollViewer? _textBoxScrollViewer;
+    private bool _isUpdatingScroll = false;
+
     public MainWindow()
     {
         InitializeComponent();
+        
+        // Use Dispatcher to ensure the visual tree is fully loaded
+        Dispatcher.UIThread.Post(InitializeScrollSynchronization, DispatcherPriority.Loaded);
+    }
+    
+    private void InitializeScrollSynchronization()
+    {
+        // Find the internal ScrollViewer of the TextBox
+        _textBoxScrollViewer = MainTextBox.GetVisualDescendants()
+            .OfType<ScrollViewer>()
+            .FirstOrDefault();
+            
+        if (_textBoxScrollViewer != null)
+        {
+            // Subscribe to scroll changes in the TextBox
+            _textBoxScrollViewer.ScrollChanged += OnTextBoxScrollChanged;
+            
+            // Also subscribe to the line numbers ScrollViewer to prevent feedback loops
+            LineNumbersScroll.ScrollChanged += OnLineNumbersScrollChanged;
+        }
+    }
+    
+    private void OnTextBoxScrollChanged(object? sender, ScrollChangedEventArgs e)
+    {
+        if (_isUpdatingScroll) return;
+        
+        _isUpdatingScroll = true;
+        
+        // Synchronize the line numbers ScrollViewer with the TextBox ScrollViewer
+        LineNumbersScroll.Offset = new Avalonia.Vector(0, _textBoxScrollViewer!.Offset.Y);
+        
+        _isUpdatingScroll = false;
+    }
+    
+    private void OnLineNumbersScrollChanged(object? sender, ScrollChangedEventArgs e)
+    {
+        // This prevents feedback loops but generally shouldn't be triggered
+        // since the line numbers ScrollViewer is set to IsHitTestVisible="False"
     }
 }
